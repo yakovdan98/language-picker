6da97a724c39927ab1cd0c17d31be2c4774b61a3
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-10-21 10:37:40 -0700

    added button start quiz funtionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+function startQuiz(event){
+  event.preventDefault();
+  document.getElementById("quiz").removeAttribute("class");
+}
 window.addEventListener("load", function () {
-    
+    this.document.getElementById("startQuiz").addEventListener("click", startQuiz)
 });
