935c2c0b4f28d392059f993a7a16820a107e0dd8
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-10-21 10:59:26 -0700

    added navigation of page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-function startQuiz(event){
-  event.preventDefault();
-  document.getElementById("quiz").removeAttribute("class");
+function startQuiz() {
+  document.getElementById("jumbotron").setAttribute("class", "jumbotron hidden");
+  let quiz = document.getElementById("quiz")
+  quiz.removeAttribute("class");
+  quiz.addEventListener("submit", processData);
 }
+
+function processData(event) {
+  event.preventDefault();
+  document.getElementById("quiz").setAttribute("class", "hidden")
+  let response = document.getElementById("output");
+  response.removeAttribute("class");
+}
+
 window.addEventListener("load", function () {
-    this.document.getElementById("startQuiz").addEventListener("click", startQuiz)
+  this.document.getElementById("startQuiz").addEventListener("click", startQuiz)
 });
